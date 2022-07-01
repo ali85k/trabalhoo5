ef2c53555f047f004635257150b5f9ac3c732ce3
ali85k <aliceanndraad56@gmail.com>
2022-07-01 10:05:29 -0300

    first-commit

---
+++ script.js
@@ -0,0 +1,18 @@
+letprimeiroArray=["assistir","um","filme","de","terror"]; 
+
+ console.log(primeiroArray); 
+
+ constnovoElemento=primeiroArray.unshift("Legal"); 
+ console.log(primeiroArray); 
+
+ constremoverElemento=primeiroArray.pop(); 
+ console.log(primeiroArray); 
+
+ constnovoElemento1=primeiroArray.push("eu","adoro"); 
+ console.log(primeiroArray); 
+
+ constremoverElemento1=primeiroArray.shift(); 
+ console.log(primeiroArray); 
+
+ letnumbers=[7,5,6,3,8,9,2,1,4]; 
+ console.log(numbers.sort());
